f04e22383b1003a1a843dcaf42339a1e9f2be571
edmilsonbraga <d1000so@unicamp.br>
2023-10-06 15:55:49 -0300

    add functions
    Signed-off-by: edmilsonbraga <d1000so@unicamp.br>

--- src/main/java/list/Pesquisa/CatalogoLivros.java
+++ src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -1,4 +1,40 @@
 package main.java.list.Pesquisa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogoLivros {
+
+    private List<Livro> livroList;
+
+    public CatalogoLivros() { this.livroList = new ArrayList<>(); }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisaPorTitulo(String titulo) {
+        List<Livro> livrosPorTitulo = new ArrayList<>();
+        if (!livroList.isEmpty()) {
+            for (Livro l : livroList) {
+                if(l.getTitulo().equalsIgnoreCase()){
+                    livrosPorTitulo.add(l);
+
+                }
+            }
+        }
+    return livrosPorTitulo;
+    }
 }
