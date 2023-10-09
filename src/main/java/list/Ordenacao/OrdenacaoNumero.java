d6b92adf3f11a77421801be27f6a423fc60c03df
edmilsonbraga <d1000so@unicamp.br>
2023-10-09 06:10:13 -0300

    Implements Numero

--- src/main/java/list/Ordenacao/OrdenacaoNumero.java
+++ src/main/java/list/Ordenacao/OrdenacaoNumero.java
@@ -2,10 +2,41 @@ package main.java.list.Ordenacao;
 
 import main.java.list.Pesquisa.Numero;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenacaoNumero {
-    private List<Numero> numeroList
+    private List<Numero> numeroList;
 
+    public OrdenacaoNumero() { this.numeroList = new ArrayList<>(); }
+
+    public void adicionarNumero(int numero) {
+        numeroList.add(new Numero(numero));
+    }
+
+    public List<Numero> numeroCrescente() {
+        List<Numero> numeroCrescenteList = new ArrayList<>(numeroList);
+        Collections.sort(numeroCrescenteList);
+        return numeroCrescenteList;
+    }
+
+    public List<Numero> numeroDecrescente() {
+        List<Numero> numeroDecrescenteList = new ArrayList<>(numeroList);
+        Collections.sort(numeroDecrescenteList, Collections.reverseOrder());
+        return numeroDecrescenteList;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumero ordenacaoNumero = new OrdenacaoNumero();
+        ordenacaoNumero.adicionarNumero(2);
+        ordenacaoNumero.adicionarNumero(10);
+        ordenacaoNumero.adicionarNumero(7);
+        ordenacaoNumero.adicionarNumero(0);
+        ordenacaoNumero.adicionarNumero(3);
+        System.out.println(ordenacaoNumero.numeroCrescente());
+        System.out.println(ordenacaoNumero.numeroDecrescente());
+    }
 }
 
